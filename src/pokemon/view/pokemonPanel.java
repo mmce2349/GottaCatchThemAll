738d27d8f7f230a6cfd524bb8114fbd1f528a4aa
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2018-01-02 09:05:01 -0700

    reduced code

--- src/pokemon/view/pokemonPanel.java
+++ src/pokemon/view/pokemonPanel.java
@@ -1,18 +1,7 @@
 package pokemon.view;
 
 import pokemon.controller.pokemonController;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.JTextArea;
-import javax.swing.SpringLayout;
-import javax.swing.JButton;
-import java.awt.Color;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.JLabel;
-import javax.swing.JScrollPane; 
-import javax.swing.JCheckBox;
-import javax.swing.JComboBox;
+import javax.swing.*;
 
 public class pokemonPanel extends JPanel
 {
