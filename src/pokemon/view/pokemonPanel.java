f7bed2dc01f8d5d1df7d3b8742a2632efda9a6c2
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2018-01-02 09:03:03 -0700

    Added labels and features to panel

--- src/pokemon/view/pokemonPanel.java
+++ src/pokemon/view/pokemonPanel.java
@@ -11,6 +11,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JLabel;
 import javax.swing.JScrollPane; 
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 
 public class pokemonPanel extends JPanel
 {
@@ -18,5 +20,35 @@ public class pokemonPanel extends JPanel
 		{
 			
 		}
+	private JLabel healthLabel;
+	private JLabel attackLabel;
+	private JLabel nameLabel;
+	private JLabel numberLabel;
+	private JLabel evolvableLabel;
+	private JLabel modifierLabel;
+	private JLabel iconLabel; 
 	
+	private JCheckBox evolvableBox;
+	private JTextField nameField;
+	private JTextField numberField;
+	private JTextField attackField;
+	private JTextField  healthField;
+	private JTextField modifierField;
+	
+	private JTextArea descriptionArea;
+	private JTextArea typeArea;
+	
+	private JButton saveButton;
+	private JButton clearButton;
+	private JComboBox pokeexDropdown;
+	
+	private JPanel firstType;
+	private JPanel secondType;
+	private JPanel thirdType;
+	private JPanel fourthType; 
+	
+	private void updatePokedexInfo(int index)
+	{
+		nameFld
+	}
 }
